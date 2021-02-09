c9c651e963c3b66a29f8ff119d77270f6c64eafa
Luciano Candaten <lucianocandaten@MacBook-Pro-de-Luciano.local>
2021-02-09 13:17:36 -0300

    teste de commit

---
+++ VersionamentoDeCodigo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VersionamentoDeCodigo
+//
+//  Created by Luciano Candaten on 09/02/21.
+//  Copyright © 2021 Luciano Candaten. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
